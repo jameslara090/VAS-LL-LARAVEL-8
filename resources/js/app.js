46874f3c116be8d8ce5af2e11c3ddd8f504482dd
james.lee.lara@gmail.com <james.lee.lara@gmail.com>
2022-11-21 11:07:00 +0700

    Add Simple Vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+window.Vue = Vue;
+
+Vue.component('Login', require('./components/Login').default)
+
+const app = new Vue({
+    el: '#app'
+})
---
+++ resources/js/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div> Hello {{ message }} </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "wellcome to vue js"
+        }
+    }
+}
+</script>
+
+<style>
+</style>
